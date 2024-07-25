c677cd56fa3b7974d5adf2479efecc91cfff0658
blasdo <72580092+blasdo@users.noreply.github.com>
2024-07-25 15:57:15 +0200

    25 de julio de 2024 15:57

--- src/player_controller.gd
+++ src/player_controller.gd
@@ -8,13 +8,12 @@ func _ready():
 	velocity.y = 0
 	position.y = 0
 	position.x = 0
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity.y += GRAVITY * delta;
 	if Input.is_action_just_pressed("Jump"):
-		var jumpMultiplier = min(max(inverse_lerp(-280.0, 250.0, position.y), 0.0), 1.0)
+		var jumpMultiplier = min(max(inverse_lerp(-300.0, 250.0, position.y), 0.0), 1.0)
 		print("jump_mul: %s" %jumpMultiplier);
 		var velMultiplier = 1.0 - min(max(inverse_lerp(0, JUMPFORCE, velocity.y), 0.0), 1.0)
 		print("vel_mul: %s" %velMultiplier);
